361bf2a199e2749ce5eed3c9544fd2d8d07c9a00
Heitor Araújo <136932197+Heitorarj@users.noreply.github.com>
2024-08-31 18:16:05 -0300

    Update Moveis.hpp

--- KangarooGame/Moveis.hpp
+++ KangarooGame/Moveis.hpp
@@ -9,10 +9,14 @@ public:
 class Jogador: public Movel {
 public:
 	sf::Texture jogadorTextura;
+	sf::Texture jogadorAgachadoTextura;
 	sf::Sprite jogadorCorpo;
+	sf::Vector2f escalaOriginal = sf::Vector2f(0.05, 0.05);
+	sf::Vector2f escalaAgachado = sf::Vector2f(0.1, 0.1); // Ajuste o fator de escala para a imagem agachada
 
 	Jogador() {
 		jogadorTextura.loadFromFile("assets/kangaroo.png");
+		jogadorAgachadoTextura.loadFromFile("assets/kangarooAgachado.png");
 		jogadorCorpo.setTexture(jogadorTextura);
 		jogadorCorpo.setScale(sf::Vector2f(0.05, 0.05));
 		jogadorCorpo.setPosition(sf::Vector2f(200, 600));
@@ -41,10 +45,16 @@ public:
 							jogadorCorpo.getPosition().y - velocidadeY));
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+			jogadorCorpo.setTexture(jogadorAgachadoTextura); // Troca a textura ao agachar
+			jogadorCorpo.setScale(escalaAgachado); // Aplica a escala para a imagem agachada
 			velocidadeY = 0.12;
 			jogadorCorpo.setPosition(
 					sf::Vector2f(jogadorCorpo.getPosition().x,
 							jogadorCorpo.getPosition().y + velocidadeY));
+		}
+		else {
+			jogadorCorpo.setTexture(jogadorTextura); // Retorna à textura original
+			jogadorCorpo.setScale(escalaOriginal); // Retorna à escala original
 		}
 	}
 
