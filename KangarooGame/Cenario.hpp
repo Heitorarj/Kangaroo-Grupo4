330fda383e6403401d6114b801cac84a83ee87df
AlessandroKSR <136772341+AlessandroKSR@users.noreply.github.com>
2024-09-01 11:47:19 -0300

    Add files via upload

--- KangarooGame/Cenario.hpp
+++ KangarooGame/Cenario.hpp
@@ -4,6 +4,7 @@
 class Cenario {
 public:
 	sf::RectangleShape retanguloCenario;
+	int largura, altura;
 };
 
 class Parede: public Cenario {
@@ -135,45 +136,17 @@ public:
 
 };
 
-class Fruta {
-public:
-	sf::Texture frutaTextura;
-	sf::Sprite frutaCorpo[3];
-
-	Fruta() {
-		for (int i = 0; i < 3; i++) {
-			frutaTextura.loadFromFile("assets/fruta.png");
-			frutaCorpo[i].setTexture(frutaTextura);
-			frutaCorpo[i].setScale(0.4, 0.4);
-			frutaCorpo[i].setOrigin(frutaCorpo[i].getLocalBounds().width / 2,
-					frutaCorpo[i].getLocalBounds().height / 2);
-		}
-
-		frutaCorpo[0].setPosition(700, 510);
-		frutaCorpo[1].setPosition(300, 420);
-		frutaCorpo[2].setPosition(600, 310);
-
-	}
-
-};
-
-class Texto: public Cenario {
+class Texto {
 public:
 
 	sf::Font fonte;
 	sf::Text texto;
 
 	Texto() {
-		retanguloCenario.setFillColor(sf::Color::Black);
-		retanguloCenario.setOutlineColor(sf::Color::Blue);
-		retanguloCenario.setOutlineThickness(5);
-		retanguloCenario.setSize(sf::Vector2f(150, 25));
-
-		fonte.loadFromFile("assets/arial_narrow_7.ttf");
+		fonte.loadFromFile("assets/x-files.ttf");
 		sf::Text meuTexto("Pontos:\t", fonte, 24);
 		texto = meuTexto;
-		texto.setPosition(sf::Vector2f(400, 760));
-		retanguloCenario.setPosition(395, 762);
+		texto.setPosition(sf::Vector2f(425, 750));
 	}
 };
 
