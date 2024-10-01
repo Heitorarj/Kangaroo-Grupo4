1fb2cbedfce85a81f84e1687de93b497b2b27492
Alessandro Kennedy <136772341+AlessandroKSR@users.noreply.github.com>
2024-10-01 19:10:21 -0300

    Sistema de vidas adicionado

---
+++ KangarooGame/PJogo.cpp
@@ -0,0 +1,71 @@
+#include "PJogo.h"
+
+//Funcoes privadas
+void Jogo::inicializaVariaveis() {
+	this->janela = nullptr;
+}
+
+void Jogo::inicializaJanela() {
+	this->janela = new sf::RenderWindow(sf::VideoMode(1100, 800),
+			"Kangaroo Game", sf::Style::Titlebar | sf::Style::Close);
+
+	this->janela->setFramerateLimit(60);
+}
+
+//Construtores / Destrutores
+Jogo::Jogo(int inputNumeroFase) {
+	this->inicializaVariaveis();
+	meuMapa.criarMapa(inputNumeroFase, &meuMapa.minhasParedes,
+			&meuMapa.minhasEscadas, &meuMapa.minhasFrutas,
+			&meuMapa.minhasArvores, &meuMapa.minhasFlores,
+			&meuMapa.minhasNuvens, &meuMapa.meuFilhote,
+			&meuMapa.minhaNuvemInimiga, janela);
+	meuTempo.updateDeltaTime();
+	this->inicializaJanela();
+
+}
+
+Jogo::~Jogo() {
+	delete this->janela;
+}
+
+const bool Jogo::jogoRodando() const {
+	return this->janela->isOpen();
+}
+
+//Funcoes
+void Jogo::pollEventos() {
+	while (this->janela->pollEvent(this->evento)) {
+		if (evento.type == sf::Event::Closed)
+			this->janela->close();
+	}
+}
+
+//Onde o jogo acontece
+void Jogo::atualiza() {
+	this->pollEventos();
+	//std::cout << meuTempo.deltaTime << "\n";
+	meuMapa.mapaUpdate(1, &jogador, meuMapa.minhasFrutas, &meuMapa.meuTexto,
+			&meuMapa.meuFilhote, meuMapa.minhasNuvens,
+			&meuMapa.minhaNuvemInimiga, meuTempo.deltaTime, janela);
+
+
+	this->inimigo.atualizaInimigo(this->janela);
+	this->jogador.atualizaJogador(this->janela);
+	this->jogador.atualizaVidas();
+
+}
+
+void Jogo::desenha(int inputNumeroFase) {
+	this->janela->clear(sf::Color(168, 231, 240));
+
+	meuMapa.desenharMapa(inputNumeroFase, meuMapa.minhasParedes,
+			meuMapa.minhasEscadas, meuMapa.minhasFrutas, meuMapa.minhasArvores,
+			meuMapa.minhasFlores, meuMapa.minhasNuvens, &meuMapa.meuFilhote,
+			&meuMapa.minhaNuvemInimiga, &meuMapa.meuTexto, janela);
+
+	this->inimigo.desenhaInimigo(this->janela);
+	this->jogador.desenhaJogador(this->janela);
+
+	this->janela->display();
+}
