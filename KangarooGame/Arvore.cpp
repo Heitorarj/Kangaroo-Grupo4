8f584a12bab6576d50999d1f199aeedd874f12ef
Alessandro Kennedy <136772341+AlessandroKSR@users.noreply.github.com>
2024-10-04 17:57:52 -0300

    Delete KangarooGame directory

--- KangarooGame/Arvore.cpp
+++
@@ -1,9 +0,0 @@
-#include "Arvore.h"
-
-Arvore::Arvore() {
-	arvoreTextura.loadFromFile("assets/arvore.png");
-	arvoreCorpo.setTexture(arvoreTextura);
-	arvoreCorpo.setScale(4, 4);
-	arvoreCorpo.setOrigin(arvoreCorpo.getLocalBounds().width / 2,
-			arvoreCorpo.getLocalBounds().height / 2);
-}
--- KangarooGame/PTempo.cpp
+++
@@ -1,9 +0,0 @@
-#include "PTempo.h"
-
-Tempo::Tempo() {
-	deltaTime = 0;
-}
-
-void Tempo::updateDeltaTime() {
-	deltaTime = deltaTimeObject.restart().asSeconds();
-}
