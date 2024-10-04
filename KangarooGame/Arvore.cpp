b1c28d7e9bb3e72c1dae9d27102d92ab043e5581
Alessandro Kennedy <136772341+AlessandroKSR@users.noreply.github.com>
2024-10-04 18:00:10 -0300

    Add files via upload

---
+++ KangarooGame/Arvore.cpp
@@ -0,0 +1,9 @@
+#include "Arvore.h"
+
+Arvore::Arvore() {
+	arvoreTextura.loadFromFile("assets/arvore.png");
+	arvoreCorpo.setTexture(arvoreTextura);
+	arvoreCorpo.setScale(4, 4);
+	arvoreCorpo.setOrigin(arvoreCorpo.getLocalBounds().width / 2,
+			arvoreCorpo.getLocalBounds().height / 2);
+}
---
+++ KangarooGame/Jogo.h
@@ -0,0 +1,41 @@
+#ifndef JOGO_H_
+#define JOGO_H_
+
+#include "Bibliotecas.h"
+#include "Inimigo.h"
+#include "Mapa.h"
+#include "Tempo.h"
+
+class Jogo {
+public:
+	//Variaveis
+	//Janela
+	sf::RenderWindow *janela;
+	sf::Event evento;
+
+	Jogador jogador;
+	Inimigo inimigo;
+	Mapa meuMapa;
+	Tempo meuTempo;
+	TelasJogo minhaTela;
+	int telaCodigo;
+
+	//Funcoes privadas
+	void inicializaVariaveis();
+	void inicializaJanela();
+
+public:
+	//Construtores / Destrtutores
+	Jogo(int inputNumeroFase);
+	~Jogo();
+
+	//
+	const bool jogoRodando() const;
+
+	//Funcoes
+	void pollEventos();
+	void atualiza();
+	void desenha(int inputNumeroFase);
+};
+
+#endif
