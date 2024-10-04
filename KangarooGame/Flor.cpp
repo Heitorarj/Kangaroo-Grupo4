fe231174f7ceb1b667a89b30cf62d54d90326c66
Alessandro Kennedy <136772341+AlessandroKSR@users.noreply.github.com>
2024-10-04 18:06:02 -0300

    Delete KangarooGame directory

--- KangarooGame/Flor.cpp
+++
@@ -1,9 +0,0 @@
-#include "Flor.h"
-
-Flor::Flor() {
-	florTextura.loadFromFile("assets/flor.png");
-	florCorpo.setTexture(florTextura);
-	florCorpo.setScale(3, 3);
-	florCorpo.setOrigin(florCorpo.getLocalBounds().width / 2,
-			florCorpo.getLocalBounds().height / 2);
-}
