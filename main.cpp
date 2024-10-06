0cde69c2ec22271dcda040815d6cf89c47f7f4c7
Guilherme Souza <136477618+alcPresunto@users.noreply.github.com>
2024-10-06 18:47:00 -0300

    Gravidade finalizada

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Jogo.h"
+
+int main(int argc, char **argv) {
+
+	//Inicia o Jogo
+	Jogo jogo(1);
+
+	while (jogo.jogoRodando()) {
+		jogo.atualiza(); //Fazer as acoes do jogo aqui, Jogo.cpp
+
+		jogo.desenha(1); //Fazer os desenhos do jogo aqui, Jogo.cpp
+	}
+
+	return 0;
+}
+
+
